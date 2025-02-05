4050982f48ec1b5fb82891c730035e4305e5264b
Shawn Deprey <shawndeprey@gmail.com>
2025-02-04 21:00:15 -0700

    Finish primary base of dialog system including animations, sound blips, etc.

--- GUI/dialog_system/scripts/dialog_system.gd
+++ GUI/dialog_system/scripts/dialog_system.gd
@@ -2,17 +2,25 @@
 @icon("res://GUI/dialog_system/icons/star_bubble.svg")
 class_name DialogSystemNode extends CanvasLayer
 signal finished
+signal letter_added(letter: String)
 
 var is_active: bool = false
+
+var text_in_progress: bool = false
+var text_speed: float = 0.03
+var text_length: int = 0
+var plain_text: String
+
 var dialog_items: Array[DialogItem]
 var dialog_item_index: int = 0
 
 @onready var dialog_ui: Control = $DialogUI
 @onready var content: RichTextLabel = $DialogUI/PanelContainer/RichTextLabel
 @onready var name_label: Label = $DialogUI/NameLabel
-@onready var portrait_sprite: Sprite2D = $DialogUI/PortraitSprite
+@onready var portrait_sprite: DialogPortrait = $DialogUI/PortraitSprite
 @onready var dialog_progress_indicator: PanelContainer = $DialogUI/DialogProgressIndicator
 @onready var dialog_progress_indicator_label: Label = $DialogUI/DialogProgressIndicator/Label
+@onready var timer: Timer = $DialogUI/Timer
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
@@ -20,6 +28,7 @@ func _ready() -> void:
 			get_parent().remove_child(self)
 			return
 		return
+	timer.timeout.connect(_on_timer_timeout)
 	hide_dialog()
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -29,6 +38,12 @@ func _unhandled_input(event: InputEvent) -> void:
 		event.is_action_pressed("attack") or
 		event.is_action_pressed("ui_accept")
 	):
+		if text_in_progress:
+			content.visible_characters = text_length
+			timer.stop()
+			text_in_progress = false
+			show_dialog_button_indicator(true)
+			return
 		dialog_item_index += 1
 		if dialog_item_index < dialog_items.size():
 			start_dialog()
@@ -53,9 +68,16 @@ func hide_dialog() -> void:
 	finished.emit()
 
 func start_dialog() -> void:
-	show_dialog_button_indicator(true)
+	show_dialog_button_indicator(false)
 	var _d: DialogItem = dialog_items[dialog_item_index]
 	set_dialog_data(_d)
+
+	# Set up text render-by-character functionality
+	content.visible_characters = 0
+	text_length = content.get_total_character_count()
+	plain_text = content.get_parsed_text()
+	text_in_progress = true
+	start_timer()
 
 func show_dialog_button_indicator(_is_visible: bool) -> void:
 	dialog_progress_indicator.visible = _is_visible
@@ -69,3 +91,17 @@ func set_dialog_data(_d: DialogItem) -> void:
 		content.text = _d.text
 	name_label.text = _d.npc_info.npc_name
 	portrait_sprite.texture = _d.npc_info.portrait
+	portrait_sprite.audio_pitch_base = _d.npc_info.dialogue_audio_pitch
+
+func start_timer() -> void:
+	timer.wait_time = text_speed
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	content.visible_characters += 1
+	if content.visible_characters <= text_length:
+		letter_added.emit(plain_text[content.visible_characters - 1])
+		start_timer()
+	else:
+		show_dialog_button_indicator(true)
+		text_in_progress = false
---
+++ interactables/dialog/scripts/dialog_portrait.gd
@@ -0,0 +1,53 @@
+@tool
+class_name DialogPortrait extends Sprite2D
+
+var blink: bool = false: set = _set_blink
+var open_mouth: bool = false: set = _set_open_mouth
+var mouth_open_frames: int = 0
+var audio_pitch_base: float = 1.0
+
+@onready var audio: AudioStreamPlayer = $"../AudioStreamPlayer"
+
+func _ready() -> void:
+	if Engine.is_editor_hint(): return
+	DialogSystem.letter_added.connect(check_mouth_open)
+	blinker()
+
+func update_portrait() -> void:
+	if open_mouth:
+		frame = 2
+	else:
+		frame= 0
+	if blink:
+		frame += 1
+
+func blinker() -> void:
+	if !blink:
+		await get_tree().create_timer(randf_range(0.1, 3)).timeout
+	else:
+		await get_tree().create_timer(0.15).timeout
+	blink = !blink
+	blinker()
+
+func _set_blink(value: bool) -> void:
+	if blink != value:
+		blink = value
+		update_portrait()
+
+func _set_open_mouth(value: bool) -> void:
+	if open_mouth != value:
+		open_mouth = value
+		update_portrait()
+
+func check_mouth_open(l: String) -> void:
+	if "aeiouy1234567890".contains(l):
+		open_mouth = true
+		mouth_open_frames += 4
+		audio.pitch_scale = randf_range(audio_pitch_base - 0.04, audio_pitch_base + 0.04)
+		audio.play()
+	elif ".,!?":
+		mouth_open_frames = 0
+	if mouth_open_frames > 0:
+		mouth_open_frames -= 1
+	if mouth_open_frames == 0:
+		open_mouth = false
